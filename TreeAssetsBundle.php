036806e722439adcfb84f3b4047929c5b4a45e96
alex290 <alex@webgoal.ru>
2018-05-10 17:53:04 +0300

    Update

---
+++ TreeAssetsBundle.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace alex290\treemanager;
+
+use yii\web\AssetBundle;
+
+/**
+ * Description of GalleryAssetsBundle
+ *
+ * @author art
+ */
+class TreeAssetsBundle extends AssetBundle {
+    
+    public $sourcePath  = '@vendor/alex290/yii2-treemanager/scr';
+    public $css = [
+        'css/main.css',
+    ];
+    public $js = [
+        'js/jquery.nestable.js',
+        'js/main.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+
+    
+}
--- TreeManager.php
+++ TreeManager.php
@@ -1,14 +1,51 @@
 <?php
 
 namespace alex290\treemanager;
+use alex290\treemanager\TreeAssetsBundle;
 
 /**
  * This is just an example.
  */
 class TreeManager extends \yii\base\Widget
 {
+    public $modelTree;
+    
     public function run()
     {
-        return "Hello!";
+        TreeAssetsBundle::register($this->view);
+        $catstree = $this->getTree();
+        debug($catstree);
+        $templ = '<div class="dd">';
+        $templ .= $this->getTreeHtml($catstree);
+        $templ .= '</div>';
+        return $templ;
+    }
+    
+    protected function getTreeHtml($treesTemp){
+        $tree = '<ol class="dd-list">';
+        foreach ($treesTemp as $treeTemp) {
+            $tree .= '<li class="dd-item dd3-item" data-id="'.$treeTemp['id'].'">';
+            $tree .= '<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">'.$treeTemp['name'].'</div>';
+            if (isset($treeTemp['childs'])){
+                $tree .= $this->getTreeHtml($treeTemp['childs']);
+            }
+            $tree .= '</li>';
+        }
+        $tree .= '</ol>';
+        return $tree;
+    }
+    
+    protected function getTree() {
+        $tree = [];
+        $catstree = $this->modelTree;
+        foreach ($catstree as $id => &$node) {
+            if (!$node['parent_id'])
+                $tree[$id] = &$node;
+            else
+                $catstree[$node['parent_id']]['childs'][$node['id']] = &$node;
+        }
+        $treeOne = $tree;
+        
+        return $treeOne;
     }
 }
