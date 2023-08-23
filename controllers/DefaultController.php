135565570b0ea35ff5f4468fef211b1cf2a3cd9d
Alexandr <alex@webgoal.ru>
2023-08-23 11:44:45 +0300

    Update Fix PHP8

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -6,29 +6,32 @@ use yii\web\Controller;
 
 class DefaultController extends Controller
 {
+
     public function actionIndex()
     {
-        if (\Yii::$app->request->isAjax) {
-            $dataPost = \Yii::$app->request->post();
+        // $dataPost = \Yii::$app->request->get();
+        $dataPost = \Yii::$app->request->post();
+
+        if (!empty($dataPost)) {
             $dataArr = json_decode($dataPost['data']);
 
-            
-            $this->saveModel($dataPost['name'], $dataArr, 0, $dataPost['numb']);
-            
+            $this->saveModel($dataPost['name'], $dataArr,  $dataPost['numb'], 0);
         }
+
         return TRUE;
     }
-    
-    protected function saveModel($patch, $data, $parent = 0, $numb) {
+
+    protected function saveModel($patch, $data, $numb, $parent = 0)
+    {
         $firstNumb = $numb;
-        
+
         foreach ($data as $value) {
             $model = $patch::find()->where(['id' => $value->id])->one();
             $model->weight = $firstNumb;
             $model->parent_id = $parent;
             $model->save();
             if (isset($value->children)) {
-                $this->saveModel($patch, $value->children, $value->id, $numb);
+                $this->saveModel($patch, $value->children, $numb, $value->id);
             }
             $firstNumb++;
         }
